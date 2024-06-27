636d722cdfc2d464993245660101ffd43e5531f5
Tsebo Letele <129738237+tsbletele@users.noreply.github.com>
2024-06-27 23:30:18 +0200

    Add files via upload

---
+++ Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeAppWPF.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string MeasurementUnit { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+    }
+}
